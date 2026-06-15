e47d88d0b7260295e39035d387e5882f830b0479
Rahmad aryadi <rahmadaryadi17@gmail.com>
2026-06-15 15:11:51 +0000

    favico nya juga menggunakan foto yang sama seperti logo website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,12 +8,25 @@ export const metadata: Metadata = {
   title: 'PaulMaker Stream | GTPS Livestream Promotion Service',
   description: 'Professional Growtopia Private Server livestream promotion. Boost your server exposure for only Rp10.000/Hour.',
   keywords: ['GTPS', 'Growtopia', 'Livestream', 'Promotion', 'PaulMaker'],
+  icons: {
+    icon: 'https://i.ibb.co.com/MxqSy3JY/Paul-Maker-Profile-Picture.png',
+    shortcut: 'https://i.ibb.co.com/MxqSy3JY/Paul-Maker-Profile-Picture.png',
+    apple: 'https://i.ibb.co.com/MxqSy3JY/Paul-Maker-Profile-Picture.png',
+  },
   openGraph: {
     title: 'PaulMaker Stream',
     description: 'Professional GTPS Livestream Promotion Service',
     url: 'https://paulmaker.stream',
     siteName: 'PaulMaker Stream',
     type: 'website',
+    images: [
+      {
+        url: 'https://i.ibb.co.com/MxqSy3JY/Paul-Maker-Profile-Picture.png',
+        width: 512,
+        height: 512,
+        alt: 'PaulMaker Stream Logo',
+      },
+    ],
   },
 };
 
