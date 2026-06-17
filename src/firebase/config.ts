25da35cb47804bc76285f04a2d40ed2ff0a25571
Rahmad aryadi <rahmadaryadi17@gmail.com>
2026-06-17 18:00:35 +0000

    kok beranda gue kek gini pas di push?
    Konfigurasi Firebase Tidak Ditemu

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyCEQiDwYCrFvVab3734RE4chnjMa5WXJPk",
+  authDomain: "studio-8560157638-2b605.firebaseapp.com",
+  projectId: "studio-8560157638-2b605",
+  storageBucket: "studio-8560157638-2b605.firebasestorage.app",
+  messagingSenderId: "1067037464109",
+  appId: "1:1067037464109:web:5eb352d80f4e7aba478aa4"
 };
 
 export const isFirebaseConfigValid = () => {
